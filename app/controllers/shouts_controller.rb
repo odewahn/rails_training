5b457a7e5b663f4b4e91a7162cb9435ffa4e7c67
William Minsinger <wminsing@yahoo.com>
2011-10-25 16:26:13 -0400

    Added ajax for the forms

--- app/controllers/shouts_controller.rb
+++ app/controllers/shouts_controller.rb
@@ -4,17 +4,22 @@ class ShoutsController < ApplicationController
   
   def index
     @shout = Shout.new
-    @shouts = Shout.page(params[:page]).per(1)
+    @shouts = Shout.page(params[:page]).per(10)
   end
 
   def create
     @shout = Shout.new(params[:shout])
     @shout.user = current_user
-    if @shout.save
-      redirect_to shouts_url, notice: "Shouted!"
-    else
-      @shouts = Shout.all
-      render "index"
+    respond_to do |format|
+      if @shout.save
+        format.html {redirect_to shouts_url, notice: "Shouted!"}
+        format.js
+      else
+        format.html do
+           @shouts = Shout.all
+           render redirect_to shouts_url
+        end
+      end
     end
   end
 end
